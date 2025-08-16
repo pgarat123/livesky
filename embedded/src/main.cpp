58acf855dff675101885c9ec2c17adca57294679
Pierre Garat <garat@et.esiea.fr>
2025-08-16 15:28:53 +0200

    Resolved communication and library issues

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,8 +1,35 @@
 #include <Arduino.h>
-void setup() {
-// write your initialization code here
+#include "DFRobot_LarkWeatherStation.h"
+#define DEVICE_ADDR                  0x42
+DFRobot_LarkWeatherStation_I2C atm(DEVICE_ADDR,&Wire);
+
+void setup(void){
+    Serial.begin(9600);
+    delay(1000);
+    while(atm.begin()!= 0){
+        Serial.println("init error");
+        delay(1000);
+    }
+    Serial.println("init success");
+    //atm.setTime(2023,3,1,17,20,0);
 }
 
-void loop() {
-// write your code here
+void loop(void){
+    Serial.println(atm.getTimeStamp());
+    Serial.print(atm.getValue("Speed").toFloat());
+    Serial.println(atm.getUnit("Speed"));
+    Serial.println(atm.getValue("Dir"));
+    Serial.print(atm.getValue("Temp").toFloat());
+    Serial.println(atm.getUnit("Temp"));
+    Serial.print(atm.getValue("Humi").toFloat());
+    Serial.println(atm.getUnit("Humi"));
+    Serial.print(atm.getValue("Pressure").toFloat());
+    Serial.println(atm.getUnit("Pressure"));
+//    Serial.print(atm.getValue("Battery"));//Available when connected to a lithium battery
+//    Serial.println(atm.getUnit("Battery")); //Available when connected to a lithium battery
+//    Serial.print(atm.getValue("Lat"));//Available when connected to a GNSS module
+//    Serial.print(atm.getValue("Lon"));//Available when connected to a GNSS module
+    Serial.println("----------------------------");
+//    Serial.println(atm.getInformation(true));
+    delay(1000);
 }
