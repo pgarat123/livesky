99b2714bb182bd401c38f10c847e0df888069ed7
Pierre Garat <garat@et.esiea.fr>
2025-08-16 15:45:39 +0200

    Renamed object for better comprehension

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,35 +1,35 @@
 #include <Arduino.h>
 #include "DFRobot_LarkWeatherStation.h"
 #define DEVICE_ADDR                  0x42
-DFRobot_LarkWeatherStation_I2C atm(DEVICE_ADDR,&Wire);
+DFRobot_LarkWeatherStation_I2C lark(DEVICE_ADDR,&Wire);
 
 void setup(void){
     Serial.begin(9600);
     delay(1000);
-    while(atm.begin()!= 0){
+    while(lark.begin()!= 0){
         Serial.println("init error");
         delay(1000);
     }
     Serial.println("init success");
-    //atm.setTime(2023,3,1,17,20,0);
+    lark.setTime(2023,3,1,17,20,0);
 }
 
 void loop(void){
-    Serial.println(atm.getTimeStamp());
-    Serial.print(atm.getValue("Speed").toFloat());
-    Serial.println(atm.getUnit("Speed"));
-    Serial.println(atm.getValue("Dir"));
-    Serial.print(atm.getValue("Temp").toFloat());
-    Serial.println(atm.getUnit("Temp"));
-    Serial.print(atm.getValue("Humi").toFloat());
-    Serial.println(atm.getUnit("Humi"));
-    Serial.print(atm.getValue("Pressure").toFloat());
-    Serial.println(atm.getUnit("Pressure"));
-//    Serial.print(atm.getValue("Battery"));//Available when connected to a lithium battery
-//    Serial.println(atm.getUnit("Battery")); //Available when connected to a lithium battery
-//    Serial.print(atm.getValue("Lat"));//Available when connected to a GNSS module
-//    Serial.print(atm.getValue("Lon"));//Available when connected to a GNSS module
+    Serial.println(lark.getTimeStamp());
+    Serial.print(lark.getValue("Speed").toFloat());
+    Serial.println(lark.getUnit("Speed"));
+    Serial.println(lark.getValue("Dir"));
+    Serial.print(lark.getValue("Temp").toFloat());
+    Serial.println(lark.getUnit("Temp"));
+    Serial.print(lark.getValue("Humi").toFloat());
+    Serial.println(lark.getUnit("Humi"));
+    Serial.print(lark.getValue("Pressure").toFloat());
+    Serial.println(lark.getUnit("Pressure"));
+//    Serial.print(lark.getValue("Battery"));//Available when connected to a lithium battery
+//    Serial.println(lark.getUnit("Battery")); //Available when connected to a lithium battery
+//    Serial.print(lark.getValue("Lat"));//Available when connected to a GNSS module
+//    Serial.print(lark.getValue("Lon"));//Available when connected to a GNSS module
     Serial.println("----------------------------");
-//    Serial.println(atm.getInformation(true));
+//    Serial.println(lark.getInformation(true));
     delay(1000);
 }
