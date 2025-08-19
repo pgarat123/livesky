7175f9c87a4bc66d2d80f848a92f8a496188c108
Pierre Garat <garat@et.esiea.fr>
2025-08-19 16:36:08 +0200

    Basic functionnality to send data to the backend

--- embedded/src/connectivity.cpp
+++ embedded/src/connectivity.cpp
@@ -1,21 +1,42 @@
-//
-// Created by Pierre Garat on 16/08/2025.
-//
 #include "connectivity.h"
+#include <Arduino.h>
+
+char server_ip[16] = "192.168.1.20"; //Default
+char server_port[6] = "5001";
+char device_id[4] = "1";
 
 void connectivitySetup()
 {
     WiFiManager wm;
-    bool res;
-    res = wm.autoConnect("AutoConnectAP"); // password protected ap
+
+    WiFiManagerParameter custom_server_ip("server_ip", "IP du serveur", server_ip, 16);
+    WiFiManagerParameter custom_server_port("server_port", "Port du serveur", server_port, 6);
+    WiFiManagerParameter custom_device_id("device_id", "ID de l'appareil", device_id, 4);
+
+    wm.addParameter(&custom_server_ip);
+    wm.addParameter(&custom_server_port);
+    wm.addParameter(&custom_device_id);
+
+    wm.setConfigPortalTimeout(180);
+
+    bool res = wm.autoConnect("LarkWeatherStationAP");
 
     if(!res)
     {
-        Serial.println("Failed to connect");
+        Serial.println("Echec de la connexion. Redemarrage...");
         ESP.restart();
     }
     else
     {
-        Serial.println("Wi-Fi connected !");
+        Serial.println("Wi-Fi connecte !");
+
+        strcpy(server_ip, custom_server_ip.getValue());
+        strcpy(server_port, custom_server_port.getValue());
+        strcpy(device_id, custom_device_id.getValue());
+
+        Serial.println("Parametres configurés :");
+        Serial.print("IP du Serveur: "); Serial.println(server_ip);
+        Serial.print("Port du Serveur: "); Serial.println(server_port);
+        Serial.print("ID de l'appareil: "); Serial.println(device_id);
     }
 }
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,40 +1,95 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "DFRobot_LarkWeatherStation.h"
 #include "connectivity.h"
-#include "WiFiManager.h"
-#define DEVICE_ADDR                  0x42
-DFRobot_LarkWeatherStation_I2C lark(DEVICE_ADDR,&Wire);
 
-void setup(void){
+#define DEVICE_ADDR 0x42
+DFRobot_LarkWeatherStation_I2C lark(DEVICE_ADDR, &Wire);
+
+// From connectivity.cpp
+extern char server_ip[16];
+extern char server_port[6];
+extern char device_id[4];
+
+const unsigned long POST_INTERVAL_MS = 300000; // 300,000 ms = 5 minutes
+unsigned long lastPostTime = 0;
+
+void sendData();
+
+void setup(void) {
     Serial.begin(9600);
     delay(1000);
+
     connectivitySetup();
-    while(lark.begin()!= 0){
-        Serial.println("init error");
+
+    while (lark.begin() != 0) {
+        Serial.println("Erreur d'initialisation du capteur, nouvel essai dans 1s");
         delay(1000);
     }
-    Serial.println("init success");
-    lark.setTime(2023,3,1,17,20,0);
-
-
+    Serial.println("Capteur initialisé avec succès.");
 }
 
-void loop(void){
-    Serial.println(lark.getTimeStamp());
-    Serial.print(lark.getValue("Speed").toFloat());
-    Serial.println(lark.getUnit("Speed"));
-    Serial.println(lark.getValue("Dir"));
-    Serial.print(lark.getValue("Temp").toFloat());
-    Serial.println(lark.getUnit("Temp"));
-    Serial.print(lark.getValue("Humi").toFloat());
-    Serial.println(lark.getUnit("Humi"));
-    Serial.print(lark.getValue("Pressure").toFloat());
-    Serial.println(lark.getUnit("Pressure"));
-//    Serial.print(lark.getValue("Battery"));//Available when connected to a lithium battery
-//    Serial.println(lark.getUnit("Battery")); //Available when connected to a lithium battery
-//    Serial.print(lark.getValue("Lat"));//Available when connected to a GNSS module
-//    Serial.print(lark.getValue("Lon"));//Available when connected to a GNSS module
+void loop(void) {
+    if (millis() - lastPostTime > POST_INTERVAL_MS) {
+        lastPostTime = millis();
+        sendData();
+    }
+}
+
+void sendData() {
     Serial.println("----------------------------");
-//    Serial.println(lark.getInformation(true));
-    delay(1000);
+    Serial.println("Lecture des donnees du capteur...");
+
+    // 1. Lecture de toutes les valeurs
+    float temp = lark.getValue("Temp").toFloat();
+    float humi = lark.getValue("Humi").toFloat();
+    float pressure = lark.getValue("Pressure").toFloat();
+    float speed = lark.getValue("Speed").toFloat();
+    String dir = lark.getValue("Dir");
+
+    Serial.printf("Temp: %.2f, Hum: %.2f, Pres: %.2f, Wind Spd: %.2f, Wind Dir: %s\n",
+                  temp, humi, pressure, speed, dir.c_str());
+
+    StaticJsonDocument<256> doc;
+
+    doc["device_id"] = atoi(device_id);
+
+    doc["temperature"] = temp;
+    doc["humidity"] = humi;
+    doc["pressure"] = pressure;
+    doc["wind_speed"] = speed;
+    doc["wind_direction"] = dir;
+
+    String jsonBuffer;
+    serializeJson(doc, jsonBuffer);
+    Serial.println("JSON cree : " + jsonBuffer);
+
+    if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+
+        String serverPath = "http://" + String(server_ip) + ":" + String(server_port) + "/api/data";
+        Serial.println("Envoi des données vers : " + serverPath);
+
+        http.begin(serverPath.c_str());
+        http.addHeader("Content-Type", "application/json");
+
+        int httpResponseCode = http.POST(jsonBuffer);
+
+        if (httpResponseCode > 0) {
+            Serial.print("Code de réponse HTTP : ");
+            Serial.println(httpResponseCode);
+            String payload = http.getString();
+            Serial.print("Reponse du serveur : ");
+            Serial.println(payload);
+        } else {
+            Serial.print("Erreur lors de l'envoi de la requête POST; code : ");
+            Serial.println(httpResponseCode);
+        }
+
+        http.end();
+    } else {
+        Serial.println("WiFi non connecte. Impossible d'envoyer les données.");
+    }
 }
